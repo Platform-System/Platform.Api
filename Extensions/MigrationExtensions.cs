3a55d8f6d8cf9859780ccb02daf39c937a3d99cd
Khánh Hưng <khanhhungzm@gmail.com>
2026-05-11 16:56:08 +0700

    refactor: remove redundant synchronous ApplyMigrations method

--- Extensions/MigrationExtensions.cs
+++ Extensions/MigrationExtensions.cs
@@ -6,13 +6,6 @@ namespace Platform.Api.Extensions;
 
 public static class MigrationExtensions
 {
-    public static void ApplyMigrations<TContext>(this IApplicationBuilder app) where TContext : DbContext
-    {
-        using var scope = app.ApplicationServices.CreateScope();
-        var context = scope.ServiceProvider.GetRequiredService<TContext>();
-        context.Database.Migrate();
-    }
-
     public static async Task ApplyMigrationsAsync<TContext>(this IApplicationBuilder app) where TContext : DbContext
     {
         using var scope = app.ApplicationServices.CreateScope();
