8c4815de32f77085964d93e6d553c857f6045a15
Khánh Hưng <khanhhungzm@gmail.com>
2026-05-11 16:53:37 +0700

    feat: add generic ApplyMigrations extension

---
+++ Extensions/MigrationExtensions.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Platform.Api.Extensions;
+
+public static class MigrationExtensions
+{
+    public static void ApplyMigrations<TContext>(this IApplicationBuilder app) where TContext : DbContext
+    {
+        using var scope = app.ApplicationServices.CreateScope();
+        var context = scope.ServiceProvider.GetRequiredService<TContext>();
+        context.Database.Migrate();
+    }
+
+    public static async Task ApplyMigrationsAsync<TContext>(this IApplicationBuilder app) where TContext : DbContext
+    {
+        using var scope = app.ApplicationServices.CreateScope();
+        var context = scope.ServiceProvider.GetRequiredService<TContext>();
+        await context.Database.MigrateAsync();
+    }
+}
